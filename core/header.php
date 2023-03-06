89a6adddee90e308676b18313a9ca389597563e0
ZashikyOff <119845760+ZashikyOff@users.noreply.github.com>
2023-03-06 11:50:17 +0100

    v0.5

--- core/header.php
+++ core/header.php
@@ -9,5 +9,7 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet">
       <script src="js/main.js" defer></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <meta name="google-site-verification" content="3c3-E1-nD5TS6t4wuvkbUhOsWvY_AU7nWgOlYn0EI1o" />
       <title>JumpOut Door <?= $title ?></title>
   </head>
