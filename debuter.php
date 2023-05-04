1bb661cf647903536bba675374ff7af95f5a302c
ZashikyOff <119845760+ZashikyOff@users.noreply.github.com>
2023-05-04 10:46:42 +0200

    FullFigureFinish

--- debuter.php
+++ debuter.php
@@ -30,7 +30,7 @@ $result = $lienDB->query($sql2);
         <div class="img">
           <?php
           if (strlen(htmlspecialchars($row['video'])) >= 1) {
-          ?><iframe width="90%" height="50%" src="<?= htmlspecialchars($row['video']); ?>" title="<?= htmlspecialchars($row['label']); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+          ?><iframe src="https://www.youtube.com/embed/<?= htmlspecialchars($row['video']); ?>" title="<?= htmlspecialchars($row['label']); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
                                                                                                                                                                                                                                                                                                 }
                                                                                                                                                                                                                                                                                                   ?>
         </div>
